39ccf79a3edb74847c1384362be2ccbe20c6194c
davidruc <davidarueda18@gmail.com>
2023-06-04 11:49:06 -0500

    feat: :memo: Métodos mágicos
    Para más información de los métodos mágios visitar la documentación

--- index.php
+++ index.php
@@ -1,34 +1,16 @@
 <?php 
 
-// Iteración de objetos
-header("Content-Type: application/json; charset:UTF-8");
+// Métodos mágicos
 
-echo "mediante una sentencia foreach es posible realizar la itereción:\n";
+echo "Son métodos especiales que sobreescriben las acciones por defecto cuando se realizan cierta acciones por defecto.\n\n";
 
-class Clase {
-    public $var1 = "Valor 1";
-    public $var2 = "Valor 2";
-    public $var3 = "Valor 3";
-    private $var_private = "variable privada";
-    protected $var_protectec = "variable protegida";
+echo "OJO Todos los nombres de mérodos que cominezan con __ son reservados de PHP. Se recomineda no utilizarlos a menos que lo que se busque sea sobreescribir el comportamiento de PHP\n\n";
 
-    function iteraVisible(){
-        foreach($this as $clave => $valor){
-            print "$clave => $valor \n";
-        }
-    }
-}
+// Los Siguientes métodos se consideran mágicos: __construct(), __destruct(), __call(), __callStatic(), __get(), __set(), __isset(), __unset(), __sleep(), __wakeup(), __serialize(), __unserialize(), __toString(), __invoke(), __set_state(), __clone(), and __debugInfo().\n\n TODOS LOS MÉTODOS MÁGIOS A EXCEPCIÓN DE __construct(), __destruct(), y __clone() deben ser declarados como públicos.
 
-//Instanciamos
-$clase = new Clase();
- 
-//Iteramos de manera externa: FORMA 1
-echo "\nIteración de manera externa (sólo puede acceder a las propiedades o métodos públicos):\n";
-foreach($clase as $clave => $valor){
-    print "$clave => $valor \n";
-}
 
-//Iteramos desde el método construido: FORMA 2
-echo "\n\nLlamando desde el método de la clase (Puede acceder a todos los datos):\n";
-$clase -> iteraVisible();
+
+// __sleep() & __wakeup(): serialize() comprueba si la clase tiene un método con el nombre mágico __sleep(). Si es así, el método se ejecuta antes de cualquier serialización. Se puede limpiar el objeto y se supone que devuelve un array con los nombres de todas las variables de el objeto que se va a serializar. Si el método no devuelve nada, entonces null es serializado y un error E_NOTICE es emitido.
+?>
+
 
