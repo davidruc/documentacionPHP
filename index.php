194293e97629469d10a010741a69ef6fc0b81b07
davidruc <davidarueda18@gmail.com>
2023-06-04 14:28:03 -0500

    feat: :memo: Comparación de objetos

--- index.php
+++ index.php
@@ -1,29 +1,8 @@
 <?php 
 
-//Palabra clave Final
+//Comparación de objetos
 header("Content-Type: application/json; charset:UTF-8");
-echo "Impide que las clases hijas sobrescriban un método, antecediendo su definición con final.\nSi la propia clase se define como final, entonces no se podrá heredar de ella. Ejemplo:\n\n";
 
-class ClasePadre {
-    public function test(){
-        echo "Llamada de ClasePadre::test()\n";
-    }
-    final public function secondTesting(){
-        echo "Llamado a ClasePadre::secondTesting()\n";
-    }
-}
+//Al usar el operador ==, se comparan de forma sencilla las variables de cada objeto. Dos instancias de un objeto son iguales si tienen los mismos atributos y valores. 
 
-
-$clase = new ClasePadre();
-$clase -> test();
-$clase -> secondTesting();
-
-//Si intento creo una clase hija que herede la padre va a generar un error fatal, el hecho de que haya utilizado la palabra final es como si cerrara por completo las opciones de herencia.
-
-// class ClaseHija extends ClasePadre {
-//     public function secondTesting(){
-//         echo "Llamado a ClaseHija::secondTesting";
-//     }
-// }  Si descomento este código se rompe lo que tengo
-
-
+//Cuando se utiliza el operador identidad ===, las variables de un objeto son idéntidas si y sólo sí hacen referencia a la misma instancia de la misma clase
