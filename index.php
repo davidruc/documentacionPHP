aac584598890c72ea278c1e240cedbbf0a2084eb
davidruc <davidarueda18@gmail.com>
2023-06-14 17:16:46 -0500

    docs: :memo: Modificadores de acceso
    se explican a fondo, algo muy sencillo

--- index.php
+++ index.php
@@ -1,65 +1,20 @@
 <?php
 
-//TODO: PHP intermedio
+//Todo: MODIFICADORES DE ACCESO en PHP
 
-//! POO 
+//*Son palabras claves para controlar la visibilidad de atributos y métodos dentro de las clases
 
-//* es un paradigma de programación que se basa en el concepto de "objetos". Los objetos son entidades que representan conceptos del mundo real y que pueden tener propiedades (atributos) y realizar acciones (métodos).
+//! En PHP hay 3 tipos de modificadores de acceso: Publico, privado y protegido
 
-//En la programación orientada a objetos, los objetos son la base fundamental y se crean a partir de clases. Una clase es una plantilla o un molde que define las propiedades y comportamientos que tendrán los objetos que se creen a partir de ella.
 
-//? PHP es un lenguaje de programación que frecuentemente está dirigida el POO 
+//? Public: Los miembros que son declarados públicos permiten ser llamados o accedidos desde cualquier parte, incluso por fuera de la clase.
 
-//* Los principales conceptos de la programación orientada a objetos son:
+//? Private: Los miembros declarados privados sólo pueden ser utilizados desde la clase que la creo.
 
-    //? Clase: Es una plantilla o definición que describe las características y comportamientos de los objetos que se pueden crear a partir de ella.
-    //? Objeto: Es una instancia de una clase. Representa un individuo o entidad específica y tiene sus propias propiedades y comportamientos.
-    //? Atributos: Son las propiedades o características de un objeto. Definen el estado de un objeto y se representan mediante variables en la clase.
-    //? Métodos: Son las acciones o comportamientos que un objeto puede realizar. Representan las operaciones que pueden realizarse con un objeto y se definen como funciones en la clase.
+//? Protected: Los miembros que son declarados protegidos pueden ser llamados desde la clase que fueron creados y desde clases heredadas. Ya por fuera si no :p
 
-//! Las copie y pegué del manual porque me gusta mucho como las definen. 
+//! Es importante elegir adecuadamente los modificadores de acceso en función de las necesidades de diseño y la seguridad de la aplicación. Un buen diseño de clases utiliza el encapsulamiento y establece un acceso controlado a los miembros, evitando el acceso directo a los datos internos desde fuera de la clase y fomentando el uso de métodos para su manipulación. 
 
-    //todo: Encapsulación: Es el principio que establece que los atributos y métodos relacionados deben agruparse en una clase para poder ocultar detalles internos y mostrar solo ciertas cosas, o una interfaz pública. 
-    //* Esto se logra con la definición de los niveles de acceso "público- public", "privado-private" o "protegido-protect"
-
-    //todo: Herencia: Es un mecanismo para crear nuevas clases en base de clases que ya existen. La clase que se utiliza como base para la creación de la otra se denomina "clase Padre" o "super clase" y por consecuencia, la otra se llama "clase hija" o "subclase". 
-    //* La herencia de clases así como en Javascript permite la utilización de métodos y propiedades públicas. En el caso del método construct siempre se puede usar por el hecho de que siempre es creado de manera pública.
-
-    //todo: Polimorfismo: Es la capacidad de un objeto de tomar diferentes formas o comportarse de diferentes maneras según el contexto. 
-    //*Esto significa que puedo tener varias clases que heredan de una misma clase base, y cada una de ellas puede tener su propia implementación del método heredado. ejemplo sencillito: 
-    
-    
-    // Clase base
-    class Animal {
-        public function hacerSonido() {
-            echo "El animal hace un sonido genérico.";
-        }
-    }
-
-    // Clase derivada
-    class Perro extends Animal {
-        public function hacerSonido() {
-            echo "El perro ladra.";
-        }
-    }
-
-    // Clase derivada
-    class Gato extends Animal {
-        public function hacerSonido() {
-            echo "El gato maulla.";
-        }
-    }
-
-    // Crear objetos de las clases derivadas
-    $animal = new Animal();
-    $perro = new Perro();
-    $gato = new Gato();
-
-    // Llamar al método hacerSonido() de cada objeto
-    $animal->hacerSonido(); // Imprime "El animal hace un sonido genérico."
-    $perro->hacerSonido(); // Imprime "El perro ladra."
-    $gato->hacerSonido(); // Imprime "El gato maulla."
-
-    //! De esta forma tengo 3 objetos iguales, que se comportan de maneras distintas gracias al polimorfismo.
+//*Básicamente manejar bien los modificadores de acceso permite darle una correcta seguridad al programa.
 
 ?>
