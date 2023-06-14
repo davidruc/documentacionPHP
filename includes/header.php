619e8a295f8cae5845618396e7ef9d8ea45b6f62
davidruc <davidarueda18@gmail.com>
2023-06-14 14:40:04 -0500

    docs: :memo: funciones include, require, include_once, require_once
    se explican con el ejemplo planteado en la documentación

---
+++ includes/header.php
@@ -0,0 +1,4 @@
+<div class="contenido">
+    <h1>Desarrollo de aplicaciones web</h1>
+    <p>Introducción a PHP</p>
+</div>
--- index.php
+++ index.php
@@ -1,36 +1,27 @@
 <?php
 
-//* Usar la línea de código 
-declare(strict_types = 1);
-//* para generar una declaración de tipos estrictos 
+// TODO: include, require, include_once, require_once
 
+//* Son funciones que se utilizan para incluir archivos externos de un programa.
 
+//? El include e include_once permite incluir un archivo PHP en el programa, la diferencia está en que el once permite la inclusión una única vez.
 
-//* Funciones que no retornan un valor. 
-
-//? Las funciones que no retornan un valor se llaman "void". Se puede usar la palabra reservada "void" para definir una función de este tipo.
-function sumar($num1, $num2): void {
-    echo $num1 + $num2;
-}
-sumar(10, 2);
-
-//! No es completamente necesario utilizar la palabra reservada void en las funciones que no retornan ningun valor
-//Todo: SIN EMBARGO, ES BUENA PRÁCTICA UTILIZARLO PORQUÉ FACILITA EL ENTENDIMIENTO DEL CÓDIGO.
-
-//*Para hacer uso de funciones que retornan un valor se utiliza la palabra reservada return. 
-
-//! SI DESEO INDICAR EL TIPO DE DATO QUE VA A RETORNAR LA FUNCIÓN DEBO PONER :? Y EL TIPO DE DATO: Ejemplo ->
-
-function usuarioAutentificado(bool $autentificado) : ?string{
-    if($autentificado){
-        return "Usuario autentificado";
-    }
-    else {
-        return null;
-    }
-}
-
-//? En el caso anterior se deberá ingresar un valor en la función booleano y retornará un valor tipo cadena de texto.
-
+//! El require y require_once, son muy similares que el include e include_once, pero con la diferencia de que si no encuentran el archivo que van a incluir detienen la ejecución del programa 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <?php include("includes/header.php")?>
+    </header>
+</body>
+</html>
