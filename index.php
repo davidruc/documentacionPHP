6f0f82ba9a055239d5ceff1fa3b80fb03ff71e23
davidruc <davidarueda18@gmail.com>
2023-06-15 18:47:22 -0500

    docs: :memo: Interfaces
    Se provee información referente a interfaces

--- index.php
+++ index.php
@@ -1,62 +1,61 @@
 <?php
 
 
+//Todo: Interfaces: 
 
-//Todo: CLASES ABSTRACTACTAS
+//* Una interfaz es una estructura que define todos los métodos que debe utilizar una clase. Es como un "contrato" que especifica los nombres de los métodos sin especificar como se implementan. ej:
 
-//? Una clase abstracta es una clase que no se puede instanciar directamente. Sirve más bien como una plantilla o base para otras clases. Sirve para definir una estructura común y los métodos deben implementarse en las clases hijas.
+    interface MiContrato{
+        public function tarea1();
+        public function tarea2(); 
 
-//! Para definir una clase abstracta se utiliza la palabra clave "abstract" antes de la declaración de la clase. 
-
-
-//TODO: Métodos abstractos: 
-
-//? Una clase abstracta puede contener métodos abstractos, también se utiliza la palabra clave abstract antes. Son métodos que no tienen implementación en la clase abstracta, deben ser implementados en las clases hijas. La declaración de un método abstracto no incluye el cuerpo del método. exp:
-
-abstract class ClaseAbstracta {
-    abstract public function metodoAbstracto();
-}
-
-//TODO: Herencia abstracta:
-
-//? Las clases hijas de una clase abstracta deben implementar todos los métodos abstractos definidos en la clase abstracta. Si una clase hija llega a no implementar todos los métodos abstractos, DEBE SER DECLARADA TAMBIÉN COMO ABSTRACTA, por lo que no puede ser instanciada y deberá ser heredada nuevamente por otra clase. UNA CLASE HIJA PUEDE EXTENDER SOLO Y ÚNICAMENTE UNA CLASE ABSTRACTA A LA VEZ. esto significa que la herencia muere directamente al utilizar una clase más.
-
-//! Las clases abstractas no  se pueden instanciar usando new. Toca instanciarlas mediante las clases hijas.
-
-//TODO: Implementación de métodos abstractos:
-
-//? Las clases hijas deben proporcionar una implementación concreta de los métodos abstractos definidos en la clase abstracta. La implementación debe tener la misma firma (nombre y parámetros ) que como se define en el método abstracto. ej:
-
-
-abstract class Animal {
-    abstract public function hacerSonido();
-}
-
-class Perro extends Animal {
-    public function __constructor(){}
-    public function hacerSonido(){
-        echo "!guau! \n\n";
     }
-}
 
-class Gato extends Animal {
-    public function __constructor(){}
-    public function hacerSonido(){
-        echo "!miau";
+    //! De manera más gráfica podemos observar el siguiente ejemplo:
+
+    interface Figura{
+        public function calcularArea();
     }
-}
 
-$perrito = new Perro();
-$michi = new Gato();
+    //? En vez de utilizar la palabra reservada para la herencia se utiliza "implements".
+    class Circulo implements Figura{
+        
+        public function __construct(private $radio){
+            $this->radio = $radio;
+        }
+        public function calcularArea(): void{
+            echo "AREA CIRCULO: ".pi()*pow($this->radio, 2);
+        }
+    }
 
-$perrito->hacerSonido();
-$michi->hacerSonido();
+    $circulo = new Circulo(5);
+    $circulo->calcularArea();
 
 
+    //* Cabe aclarar que si se puede hacer herencia de interfaces utilizando la pablabra clave extends, esto sirve para agregar nuevos métodos a interfaces ya existentes.
 
-//! ---- REVISAR BIEN EL ARCHIVO DE PERSONA PARA ENTENDER MEJOR LOS PROCEDIMIENTOS REALIZADOS ---- !
+    //Un ejemplo es esto es el siguiente 
 
- 
+    interface Figura3D extends Figura {
+        public function calcularVolumen();
+    }
 
+    
+    class Cuadrado implements Figura3D{
+        
+        public function __construct(private $lado){
+            $this->lado = $lado;
+        }
+        public function calcularArea(): void{
+            echo "<br><br> AREA CUADRADO: ". 6* pow($this->lado, 2)."<br><br>";
+        }
+        public function calcularVolumen(): void{
+            echo "VOLUMEN CUADRADO: ".pow($this->lado, 3);
+        }
+    }
+
+    $cuadrado = new Cuadrado(5);
+    $cuadrado->calcularArea();
+    $cuadrado->calcularVolumen();
 
 ?>
